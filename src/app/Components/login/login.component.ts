77d89be66f8f8bba5bb02546ef47366f0c4def40
Alfred Pithu <alfredpithu@gmail.com>
2024-01-06 09:09:58 +0600

    merging

--- src/app/Components/login/login.component.ts
+++ src/app/Components/login/login.component.ts
@@ -1,10 +1,5 @@
 import { Component } from '@angular/core';
-<<<<<<< HEAD
-import { LoginServiceService } from '../../Services/login-service.service';
-import { ILoginData } from '../../Interfaces/LoginDataInterface';
-=======
 import { SkeletonApiService } from '../../services/skeleton-api/skeleton-api.service';
->>>>>>> 64dc3e058bb694b256c0af4f24e2194b2de42a92
 
 @Component({
   selector: 'app-login',
@@ -12,30 +7,6 @@ import { SkeletonApiService } from '../../services/skeleton-api/skeleton-api.ser
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
-<<<<<<< HEAD
-  constructor(private loginService: LoginServiceService) {}
-
-  email?: string;
-  password?: string;
-
-  passwordVisible = false;
-
-  postData(): void {
-    if (this.email && this.password) {
-      const data: ILoginData = {
-        email: this.email,
-        password: this.password,
-      };
-
-      this.loginService.sendLoginDataToBackend(data).subscribe((res) => {
-        console.log(res);
-        this.email = '';
-        this.password = '';
-      });
-    } else {
-      alert('Please Provide Email and Password');
-    }
-=======
   email: string = '';
   password: string = '';
 
@@ -47,8 +18,11 @@ export class LoginComponent {
 
   onSubmit() {
     this.api.login(this.email, this.password).subscribe({
-      next: () => console.log('success')
-    })
->>>>>>> 64dc3e058bb694b256c0af4f24e2194b2de42a92
+      next: () => console.log('success'),
+    });
   }
 }
+
+
+
+
--- src/app/services/skeleton-api/skeleton-api.service.ts
+++ src/app/services/skeleton-api/skeleton-api.service.ts
@@ -1,18 +1,18 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { UserInterface } from '../../Interfaces/UserInterface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SkeletonApiService {
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   // Move this to an environment variable later.
   rootUrl = 'http://localhost:5000';
 
-  login (email: string, password: string) : Observable<{status: string}> {
-    return this.http.post<{status: string}>(this.rootUrl + '/auth/login', { email, password });
+  login(email: string, password: string): Observable<{ status: string }> {
+    return this.http.post<{ status: string; user: UserInterface }>(this.rootUrl + '/auth/login',{ email, password });
   }
 }
