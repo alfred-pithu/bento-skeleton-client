783454bdcfc749dbef739061be7266eb812443e1
Samiya Kazi <samiya.kazi09@gmail.com>
2024-01-08 11:31:43 +0600

    fix: environment variables

--- src/app/services/skeleton-api/skeleton-api.service.ts
+++ src/app/services/skeleton-api/skeleton-api.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { IUser } from '../../Interfaces/user.interface';
-import { environment } from '../../../environments/environment.development';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
